da02633c1fbe4f620560dfcf0f99879ea845993f
shuvo90901 <shuvo90901>
2022-10-27 13:18:31 +0600

    blog page complete

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Blog = () => {
     return (
-        <div>
-            <h2>itle</h2>
+        <div className='pt-5'>
+            <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header >what is cors?</Accordion.Header>
+                    <Accordion.Body className='text-success'>
+                        Cross-origin resource sharing (CORS) is a browser mechanism which enables controlled access to resources located outside of a given domain. It extends and adds flexibility to the same-origin policy (SOP). However, it also provides potential for cross-domain attacks, if a website's CORS policy is poorly configured and implemented. CORS is not a protection against cross-origin attacks such as cross-site request forgery (CSRF).
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Why are you using firebase? What other options do you have to implement authentication?</Accordion.Header>
+                    <Accordion.Body className='text-success'>
+                        The Firebase Realtime Database lets you build rich, collaborative applications by allowing secure access to the database directly from client-side code. Data is persisted locally, and even while offline, realtime events continue to fire, giving the end user a responsive experience.Have a other option to authentication:<br />
+                        1.Auth0,<br />
+                        2. MongoDB,<br />
+                        3. Passport,<br />
+                        4. Okta.
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>How does the private route work?</Accordion.Header>
+                    <Accordion.Body className='text-success'>
+                        The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in)
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                    <Accordion.Header>What is Node? How does Node work?</Accordion.Header>
+                    <Accordion.Body className='text-success'>
+                        Node. js is a JavaScript runtime environment that achieves low latency and high throughput by taking a “non-blocking” approach to serving requests. In other words, Node. js wastes no time or resources on waiting for I/O requests to return.<br />
+                        It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
         </div>
     );
 };
